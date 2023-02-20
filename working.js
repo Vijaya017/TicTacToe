3d00f7c2d43460e9baa11db463a6da43cf385ac0
Vijaya Sinha <sinhavijaya20@gmail.com>
2023-02-20 15:21:37 +0530

    added features

--- working.js
+++ working.js
@@ -1,40 +1,66 @@
 const boxes = document.querySelectorAll('.box')
 const restart = document.querySelector('.restart')
-let available_moves = [0,1,2,3,4,5,6,7,8]
+const gameover = document.querySelector('#gameover')
+const easy = document.querySelector('#easy')
+const hard = document.querySelector('#hard')
+const close =document.querySelector(".close")
+const next = document.querySelector("#round")
+const count=document.querySelector(".count")
+const newG = document.querySelector('#newG')
 
+let available_moves = [0,1,2,3,4,5,6,7,8]
 let X_moves=[]
 let O_moves=[]
+let winning_boxes = []
+let difficulty = 0
+let round = 1
+let timer
 
-const winning_Code=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+const winning_Code = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-boxes.forEach(box=>{box.addEventListener('click',boxclicked)})
-restart.addEventListener('click',restartit)
+easy.addEventListener('click', modeselect)
+hard.addEventListener('click', modeselect)
+close.addEventListener('click', startGame)
+next.addEventListener('click', startRound)
+newG.addEventListener('click', newgame)
 
+function startGame(){
+    document.querySelector('#intro').style.display = 'none'
+    count.style.display="flex"
+    startRound()
+}
+
+function startRound(){
+    restartit()
+    boxes.forEach(box=>{box.addEventListener('click', boxclicked)})
+    restart.addEventListener('click', restartit)
+    count.innerHTML='Round '+round 
+}
+//console.log(boxes)
 
 function boxclicked(e){
     let user_move = Number(e.target.id)
-    if(available_moves.includes(user_move) && available_moves.length>0){
+    if(available_moves.includes(user_move) && available_moves.length > 0){
         e.target.innerHTML="X"
         X_moves.push(user_move)
-        available_moves.splice(available_moves.indexOf(user_move),1)
-        
+        available_moves.splice(available_moves.indexOf(user_move), 1)
+
         if(winnerexists(X_moves)){
-            alert("User Won!")
-            setTimeout(restartit,2000)
+            endAnimation("You won!")
             return
         }
 
         if(available_moves.length === 0){
-            alert("Draw Match!")
+            endAnimation("It's a draw!")
             return
         }
 
@@ -44,8 +70,7 @@ function boxclicked(e){
         available_moves.splice(available_moves.indexOf(pc_moves),1)
 
         if(winnerexists(O_moves)){
-            alert("Computer Won!")
-            setTimeout(restartit,2000)
+            endAnimation("I won!")
             return
         }
     }
@@ -56,20 +81,61 @@ function winnerexists(moves){
         let counter=0
         for(let i=0;i<8;i++){
             for(let j =0;j<3;j++){
-                if(moves.includes(winning_Code[i][j]))
-                counter++;
+                if(moves.includes(winning_Code[i][j])){
+                    winning_boxes.push(winning_Code[i][j])
+                    counter++;
+                }
             }
             if(counter === 3)
-            return true
+                return true
             counter = 0
+            winning_boxes = []
         }
         return false
 }
 
-function restartit(){
-    boxes.forEach(box =>{box.innerHTML=""})
-    available_moves = [0,1,2,3,4,5,6,7,8]
-    X_moves=[]
-    O_moves=[]
+function endAnimation(text){
+    boxes.forEach(box => {box.removeEventListener('click', boxclicked)})
+    round++
+
+    for(let i = 0; i < 9; i++){
+        let box = boxes[i]
+        if(winning_boxes.includes(i)){
+            box.classList.add('popOut')
+        }
+    }
+
+    timer = setTimeout(function(){
+        gameover.style.display = 'grid'
+        gameover.firstElementChild.textContent = text
+    }, 3000)
+    if(round > 3){
+        next.style.display = 'none'
+    }
+//    setTimeout(restartit, 5000)
 }
 
+function restartit(){
+    boxes.forEach(box =>{
+        box.innerHTML = ""
+        box.classList.remove('fadeOut')
+        box.classList.remove('popOut')
+        box.addEventListener('click', boxclicked)
+    })
+    clearTimeout(timer)
+    gameover.style.display = 'none'
+    available_moves = [0,1,2,3,4,5,6,7,8]
+    X_moves = []
+    O_moves = []
+    winning_boxes = []
+}
+function newgame(){
+    restartit()
+    next.style.display = 'flex'
+    round = 1
+    startGame()
+}
+
+function modeselect(){
+
+}
